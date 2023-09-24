fe793c38d3d3d1415281a585d82df0d8774fdd9e
Eros Cedeño <eycer1@gmail.com>
2023-09-24 19:36:57 -0400

    Documentation

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,7 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { SharedService } from '../shared.service';
-
+/**
+ * Authentication Guard
+ * 
+ * Guard to check if user is logged in or not
+ * Send a request to server to refresh the token and if the token is valid, return true
+ * If the token is not valid, redirect to login page and return false
+ */
 export const authGuard: CanActivateFn = async (route, state) => {
   const router = inject(Router);
   const services = inject(SharedService);
